3cec5e34319b004f3afcc216b34257e81efdb556
Ziver Koc <ziver.koc@ericsson.com>
2019-02-20 11:17:03 +0100

    Removed target folder and added a gitignore file

--- target/classes/bad.sh
+++
@@ -1,3 +0,0 @@
-for line in $(ls); do
-	echo line
-done	
--- target/classes/scriptAnalyzer.ps1
+++
@@ -1,6 +0,0 @@
-param( 
-[string]$inputDir,
-[string]$output
-)
-Import-Module PSScriptAnalyzer;
-(Invoke-ScriptAnalyzer -Path "$inputDir" -Recurse | Select-Object RuleName, Message, Line, Column, Severity, @{Name='File';Expression={$_.Extent.File }} | ConvertTo-Xml).Save("$output")
